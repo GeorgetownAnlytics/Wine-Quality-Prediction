9f15dd27ddd8395df48aea9049156d8e3758ef3f
mw1296 <93088689+mw1296@users.noreply.github.com>
2023-11-02 21:36:24 -0400

    update app.py

--- Project/ApplicationTutorial/app/app.py
+++ Project/ApplicationTutorial/app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template,request
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pickle
 from sklearn.model_selection import train_test_split, cross_val_score
 import plotly.express as px
@@ -21,7 +23,7 @@ def hello_world():
     else:
         text = request.form['text']
         random_string = uuid.uuid4().hex
-        path = "static_image/"+random_string+'.svg'
+        path = "static/"+random_string+'.svg'
         # model = load('model.joblib')
         # np_arr = floats_string_to_np_arr(text)
         # make_picture('AgesAndHeight.pkl', model, np_arr, path)
@@ -43,8 +45,8 @@ def hello_world():
         return render_template('index.html', href=path)
 
 # plot for the model with output
-def plot_graphs(model, new_input_arr, output_file_static, output_file_interactive):
-    df = gr8_wq
+def plot_graphs(model, new_input_arr, output_file):
+    df = pd.read_csv("dataset/winequality-red.csv",header=0)
     fig = px.scatter_3d(df, x='alcohol', y="volatile acidity", z='quality', color='quality',color_continuous_scale='bluered')
     fig.update_traces(marker=dict(size=5))
     fig.update_layout(legend=dict(
@@ -83,26 +85,27 @@ def plot_graphs(model, new_input_arr, output_file_static, output_file_interactiv
     )
     )
     print('\n' + "Wine Quality vs. Predict Wine Quality (yellow) Graph For The Input Wine Features Is Displayed Below")
-    fig.write_image(output_file_static, engine='kaleido')
-    fig.write_html(output_file_interactive)
+    fig.write_image(output_file, engine='kaleido')
     fig.show()
-
-    # output feature importance - using Random Forest 
-    gr8_X_train.columns
-    model.estimators_[1].feature_importances_
-    feat_df = pd.DataFrame({'Feature':gr8_X_train.columns.array,'Importance':model.estimators_[1].feature_importances_})
-
-    # graph feature important
-    plt.figure(figsize=(20,8))
-    sns.barplot(y=feat_df['Importance'], x=feat_df['Feature'], palette='flare').set(title='Features Importance to predict Wine Quality')
-    plt.show()
-
 
 
 # Define input function
 def floatsome_to_np_array(floats_str):
-
     floats = np.array([float(x) for x in floats_str.split(',')])
     return floats.reshape(len(floats),1)
-
 floatsome_to_np_array("1, 222, 3")
+
+
+
+# Graph feature importance
+df=pd.read_csv("dataset/winequality-red.csv",header=0)
+pkl_filename='TrainedModel/StackedPickle.pkl'
+with open(pkl_filename, 'rb') as file:
+    model=pickle.load(file)
+    # output feature importance - using Random Forest 
+feat_df = pd.DataFrame({'Feature':df.columns[0:11],'Importance':model.estimators_[1].feature_importances_})
+
+plt.figure(figsize=(20,8))
+sns.barplot(y=feat_df['Importance'], x=feat_df['Feature'], palette='flare').set(title='Features Importance to predict Wine Quality')
+    # save feature importance file
+plt.savefig("static/feature_importance.png")
